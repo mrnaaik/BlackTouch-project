06d20b958f0a28188e271d49f74fb9c265e95d4c
NENAVATH NAGENDRA NAIK <nnagendranaik2001@gmail.com>
2025-04-01 09:36:57 +0530

    Add files via upload

--- testcases/test_data/conftest.py
+++ testcases/test_data/conftest.py
@@ -2,5 +2,5 @@ import pytest
 from selenium import webdriver
 @pytest.fixture()
 def setup():
-    driver = webdriver.Edge("C:\\Drive\\msedgedriver.exe")
+    driver=webdriver.Edge("C:\\users\\edgedriver_win64\\msedgedriver.exe")
     return driver
